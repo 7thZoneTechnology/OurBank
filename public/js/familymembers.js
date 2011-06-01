172532e193160756778eef3e231c4e2d4b4fa1f2
prakash83 <prakash.g@mahiti.org>
2011-06-01 14:54:01 +0530

    add validation for unique uid for family member in js folder

--- public/js/familymembers.js
+++ public/js/familymembers.js
@@ -35,6 +35,7 @@ $(document).ready(
         }
         newRow.find("input:text").val("");
         newRow.find("input:checkbox").removeAttr('checked');
+        newRow.find("input:checkbox").removeAttr('disabled');
         newRow.find("input:hidden").removeAttr('value');
         newRow.find("input:radio").removeAttr('checked');
         newRow.find('span').remove();
@@ -45,19 +46,18 @@ $(document).ready(
                        dateFormat: 'dd/mm/yyyy',
                        onSelect: updateSelected,
                        parseDate:'dd/mm/yyyy',
-                       commandsAsDateFormat:true,});
+                       commandsAsDateFormat:true,
+                        maxDate: '+0D',});
         });
-
-        $('')
-        arrangeCheckboxNames();
         arrangeRadioNames();
         arrangedateid();
+//         arrangecheckbox();
       }
     );
 
     $('tr input[type=text]').focus(
       function() {
-        $(this).addClass('myFocused');   
+        $(this).addClass('myFocused');
       }
     ).blur(
       function() {
@@ -65,20 +65,81 @@ $(document).ready(
       }
     );
 
-    var arrangeCheckboxNames = function() {
-    var length = 1;
-    $('tr#tmp').find('input:checkbox').val(length);
 
-
-    length++;
-    $('tr#tmp').nextAll('tr').each( function() {
-    $(this).find('input:checkbox').val(length);
-        length++;
+    $(".entitleid1").click ( function() {
+         var entitlename=$(this).attr("name").split("-"); 
+        if($(".entitlenone-"+entitlename[1]).is(':checked')) {
+                $(".entitlevalid-"+entitlename[1]).attr ( "disabled" , true );
+		$(".entitlevalid-"+entitlename[1]).removeAttr('checked');
+        } else {
+                $(".entitlevalid-"+entitlename[1]).attr ( "disabled" , false );
+        }
     });
-};
 
 
-    var arrangedateid = function() { 
+    $(".entitleid").click ( function() {
+         var entitlename=$(this).attr("name").split("-"); 
+        if($(".entitlevalid-"+entitlename[1]).is(':checked')) {
+                $(".entitlenone-"+entitlename[1]).attr ( "disabled" , true );
+		$(".entitlenone-"+entitlename[1]).removeAttr('checked');
+        } else {
+                $(".entitlenone-"+entitlename[1]).attr ( "disabled" , false );
+        }
+    });
+
+    $(".uidvalid").change( function() {
+	var id=$(this).attr("id").split("-"); 
+        if($(this).val())
+        {  uid=$(this).val();
+           path=$('#baseurl').val();
+           targeturl=path+"/familymembers/index/checkuid?uid="+uid+"&hiddenid="+id[1]; //alert(targeturl);
+	   $.ajax({ url: targeturl, success: function(data){ $('#uiddiv-'+id[1]).html(data) }});
+        }
+// 	alert($('#hiddenuid-'+id[1]).val());
+    }); 
+
+    $(".uidvaltext").change( function() {
+	var id=$(this).attr("id").split("-");
+        var uidvalue = $(this).val();
+        habitfields = $("input[name='uid"+"[]']").serializeArray(); 
+        len = habitfields.length; //alert(len);
+        for(i=1; i<=len; i++){ //alert(i);
+        if(i!=id[1]){
+        if($('#uid-'+i).val()!=uidvalue)
+        {
+            $(this).next('span#uidmember').remove();
+        }
+        else
+        {
+            $(this).after('<span id="uidmember" style="color: #FF0000">UID repeating</span>');
+            return false;
+        }
+        }
+        }
+    });
+
+
+    $(".profid1").click ( function() {
+         var entitlename=$(this).attr("name").split("-"); 
+        if($(".profidnone-"+entitlename[1]).is(':checked')) {
+                $(".profidvalid-"+entitlename[1]).attr ( "disabled" , true );
+		$(".profidvalid-"+entitlename[1]).removeAttr('checked');
+        } else {
+                $(".profidvalid-"+entitlename[1]).attr ( "disabled" , false );
+        }
+    });
+
+    $(".profid").click ( function() {
+         var entitlename=$(this).attr("name").split("-"); 
+        if($(".profidvalid-"+entitlename[1]).is(':checked')) {
+                $(".profidnone-"+entitlename[1]).attr ( "disabled" , true );
+		$(".profidnone-"+entitlename[1]).removeAttr('checked');
+        } else {
+                $(".profidnone-"+entitlename[1]).attr ( "disabled" , false );
+        }
+    });
+
+    var arrangedateid = function() {
     var length = 1;
     $('tr#tmp').find('input.datechange').attr( 'id', 'date-'+length );
     $('tr#tmp').find('input.agechange').attr( 'id', 'age-'+length );
@@ -86,29 +147,47 @@ $(document).ready(
     $('tr#tmp').find('select.bankfilter').attr( 'id', 'bank-'+length );
     $('tr#tmp').find('div.cboclass').attr( 'id', 'cbodiv-'+length );
     $('tr#tmp').find('div.bankclass').attr( 'id', 'bankdiv-'+length );
+    $('tr#tmp').find('div.uidclass').attr( 'id', 'uiddiv-'+length );
     $('tr#tmp').find('select.cbofilter').attr( 'id', 'cbopromoter-'+length );
     $('tr#tmp').find('select.findbank').attr( 'id', 'accounttype-'+length );
     $('tr#tmp').find('select.interest').attr( 'id', 'source-'+length );
+    $('tr#tmp').find('input.uidvalid').attr('id', 'uid-'+length);
     $('tr#tmp').find('input.disableid').attr( 'id', 'disable-'+length );
     $('tr#tmp').find('input.disableamt').attr( 'id', 'amtdisable-'+length );
-    $('tr#tmp').find('select.entitleid').attr( 'name', 'entitle-'+length+'[]');
-    $('tr#tmp').find('select.profid').attr( 'name', 'profession-'+length+'[]');
+    $('tr#tmp').find('input.entitleid').attr( 'name', 'entitle-'+length+'-[]');
+    $('tr#tmp').find('input.entitleid1').attr( 'name', 'entitle-'+length+'-[]');
+    $('tr#tmp').find('input.entitleid1').attr( 'class', 'entitleid1 entitlenone-'+length);
+    $('tr#tmp').find('input.entitleid').attr( 'class', 'entitleid entitlevalid-'+length);
+
+    $('tr#tmp').find('input.profid').attr( 'name', 'profid-'+length+'-[]');
+    $('tr#tmp').find('input.profid1').attr( 'name', 'profid-'+length+'-[]');
+    $('tr#tmp').find('input.profid1').attr( 'class', 'profid1 profidnone-'+length);
+    $('tr#tmp').find('input.profid').attr( 'class', 'profid profidvalid-'+length);
     length++;
 
-    $('tr#tmp').nextAll('tr').each( function() {;
+    $('tr#tmp').nextAll('tr').each( function() {
         $(this).find('input.datechange').attr( 'id', 'date-'+length );
         $(this).find('input.agechange').attr( 'id', 'age-'+length );
         $(this).find('div.branchclass').attr( 'id', 'branchdiv-'+length );
         $(this).find('select.bankfilter').attr( 'id', 'bank-'+length );
         $(this).find('div.cboclass').attr( 'id', 'cbodiv-'+length );
         $(this).find('div.bankclass').attr( 'id', 'bankdiv-'+length );
+    	$(this).find('div.uidclass').attr( 'id', 'uiddiv-'+length );
         $(this).find('select.cbofilter').attr( 'id', 'cbopromoter-'+length );
         $(this).find('select.findbank').attr( 'id', 'accounttype-'+length );
         $(this).find('select.interest').attr('id', 'source-'+length);
         $(this).find('input.disableid').attr('id', 'disable-'+length);
         $(this).find('input.disableamt').attr('id', 'amtdisable-'+length);
-        $(this).find('select.entitleid').attr( 'name', 'entitle-'+length+'[]');
-        $(this).find('select.profid').attr( 'name', 'profession-'+length+'[]');
+        $(this).find('input.uidvalid').attr('id', 'uid-'+length);
+        $(this).find('input.entitleid').attr( 'name', 'entitle-'+length+'-[]');
+        $(this).find('input.entitleid1').attr( 'name', 'entitle-'+length+'-[]');
+    	$(this).find('input.entitleid1').attr( 'class', 'entitleid1 entitlenone-'+length);
+        $(this).find('input.entitleid').attr( 'class', 'entitleid entitlevalid-'+length);
+
+	$(this).find('input.profid').attr( 'name', 'profid-'+length+'-[]');
+	$(this).find('input.profid1').attr( 'name', 'profid-'+length+'-[]');
+	$(this).find('input.profid1').attr( 'class', 'profid1 profidnone-'+length);
+	$(this).find('input.profid').attr( 'class', 'profid profidvalid-'+length);
         length++;
     });
     };
@@ -132,18 +211,25 @@ $(document).ready(
     });
 
     $('.agechange').keyup(function() {
-        var age = $(this).val();
+        var age = $(this).val(); //alert(age);
+        var id=$(this).attr("id").split("-");
+        if(age<=120){
+        $(this).next('span').remove();
         var currentdate=$('#currentdate').attr("value").split("/");
         var dobyear = currentdate[2]-age;
         var dob = currentdate[0]+'/'+currentdate[1]+'/'+dobyear;
-        var id=$(this).attr("id").split("-");
         $('#date-'+id[1]).val(dob);
+        }
+        else {
+            $(this).after('<span style="color: #FF0000">Age less than 120</span>');
+            $('#date-'+id[1]).val('');
+        }
     });
 
     $('.interest').change(function() {
         var id=$(this).attr("id").split("-");
         var selectvalue=$(this).val();
-        if(selectvalue==4){
+        if(selectvalue==2){
         $('#disable-'+id[1]).hide(); 
         $('#disable-'+id[1]).removeAttr('value');
         }
@@ -151,7 +237,7 @@ $(document).ready(
         $('#disable-'+id[1]).show();
         }
 
-        if(selectvalue==3){
+        if(selectvalue==6){
         $('#amtdisable-'+id[1]).hide();
         $('#amtdisable-'+id[1]).removeAttr('value');
         }
@@ -160,7 +246,7 @@ $(document).ready(
         }
     })
 
-    $('.bankfilter').change(function() { alert('Hi prakash');
+    $('.bankfilter').change(function() {
         var id=$(this).attr("id").split("-");
         if($(this).val())
         {  bank_id=$(this).val();
@@ -185,7 +271,7 @@ $(document).ready(
         if($(this).val())
         {  type_id=$(this).val();
            path=$('#baseurl').val();
-           targeturl=path+"/familymembers/index/getbank?type_id="+type_id+"&divid="+id[1]; alert(targeturl);
+           targeturl=path+"/familymembers/index/getbank?type_id="+type_id+"&divid="+id[1];
 	   $.ajax({ url: targeturl, success: function(data){ $('#bankdiv-'+id[1]).html(data) }});
         }
     });
@@ -193,6 +279,7 @@ $(document).ready(
     $('.date').datepick({
      format: 'dd/mm/yyyy',
      onSelect: updateSelected,
+     maxDate: '+0D',
     });
 
     function updateSelected(date) {
@@ -229,16 +316,29 @@ $(document).ready(
                 }
         });
 
+     $(this).find(':text.noncharacters').each(function() {
+          var regex = new RegExp(/^[a-zA-Z\ \']+$/);
+          if(!regex.test($(this).val()) && $(this).val() != "") {
+              if($(this).next('span').length == 0 ) {
+                 $(this).after('<span id="charid" style="color: #FF0000"><br>Alphanumeric not allowed</span>');
+              }
+              e.preventDefault();
+              return;
+          } else {
+                  $(this).next('span#charid').remove();
+          }
+      });
+
         $(this).find(':text.numeric').each(function() {
-           var regex = new RegExp(/^[1-9][0-9]*$/);
+           var regex = new RegExp(/^-?(?:\d+|\d{1,3}(?:,\d{3})+)(?:\.\d+)?$/);
            if(!regex.test($(this).val()) && $(this).val() != "") {
                if($(this).next('span').length == 0 ) {
-                  $(this).after('<span style="color: #FF0000">Numeric Data allowed only.</span>');
+                  $(this).after('<span style="color: #FF0000" id="numberspan">Alphanumeric not allowed</span>');
                }
                e.preventDefault();
                return;
            } else {
-                   $(this).next('span').remove();
+                   $('span#numberspan').next('span').remove();
            }
        });
 
