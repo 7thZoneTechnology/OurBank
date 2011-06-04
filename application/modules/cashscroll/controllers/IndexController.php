ab36f71d59fd0f297a9388e09c18229fc7bf2a79
jebastinej <jjebastine@gmail.com>
2011-06-04 13:50:52 +0530

    cash scroll

--- application/modules/cashscroll/controllers/IndexController.php
+++ application/modules/cashscroll/controllers/IndexController.php
@@ -30,17 +30,7 @@ class Cashscroll_IndexController extends Zend_Controller_Action
         $this->view->pageTitle = $this->view->translate("Cash scroll");
         $this->view->title =  $this->view->translate('Reports');
         $this->view->type = $this->view->translate("financialReports");
-		$this->view->adm = new App_Model_Adm();
-	$globalsession = new App_Model_Users();
-                $this->view->globalvalue = $globalsession->getSession();// get session values
-                $this->view->createdby = $this->view->globalvalue[0]['id'];
-                $this->view->username = $this->view->globalvalue[0]['username'];
-	
-	$storage = new Zend_Auth_Storage_Session();
-        $data = $storage->read();
-        if(!$data){
-            $this->_redirect('index/login');
-        }
+$this->view->adm = new App_Model_Adm();
     }
 	//view action
     public function indexAction() 
